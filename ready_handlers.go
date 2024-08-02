6aed28843881c5cb014c3fbbb8ea7eb56c29f48f
Saralytics <zhaoxue.li@outlook.com>
2024-08-02 12:05:25 +0800

    complete the systeam readiness handlers

--- ready_handlers.go
+++ ready_handlers.go
@@ -2,10 +2,16 @@ package main
 
 import "net/http"
 
-func readinessHandler(http.ResponseWriter, *http.Request) {
-
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	type resp struct {
+		Response string `json:"resp"`
+	}
+	respondWithJSON(w, http.StatusOK, resp{
+		Response: "System ready",
+	})
 }
 
-func errorHandler(http.ResponseWriter, *http.Request) {
+func errorHandler(w http.ResponseWriter, r *http.Request) {
 
+	respondWithError(w, http.StatusInternalServerError, "System not ready")
 }
