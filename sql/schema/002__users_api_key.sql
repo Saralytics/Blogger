99b24f200593748f1abd11611080b0e8328b13ef
Saralytics <zhaoxue.li@outlook.com>
2024-08-19 15:20:04 +0800

    regenerated the code after schema migration

--- sql/schema/002__users_api_key.sql
+++ sql/schema/002__users_api_key.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 ALTER TABLE users 
-ADD api_key varchar(64) default encode(sha256(random()::text::bytea), 'hex');
+ADD api_key varchar(64) not null default encode(sha256(random()::text::bytea), 'hex');
 
 -- +goose Down
 ALTER TABLE users DROP api_key;
